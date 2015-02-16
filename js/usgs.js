dcce716c1d971db0e002a3ea7b6e20865d9ccca3
Alan Dingeldein <kd5zex@gmail.com>
2015-02-15 19:26:04 -0600

    general updates and new pics

---
+++ js/usgs.js
@@ -0,0 +1,42 @@
+
+var stations = '&countyCd=48091';
+
+var request = $.ajax({
+    url: 'http://waterservices.usgs.gov/nwis/iv/?format=json' + stations 
+});
+
+request.done(function (results) {
+    buildFrame(results);
+    for (i = 0; i < results.value.timeSeries.length; ++i) {
+        stationCode = results.value.timeSeries[i].sourceInfo.siteCode[0].value;
+        dataLoc = results.value.timeSeries[i].sourceInfo.siteName;
+        dataPoint = results.value.timeSeries[i].values[0].value[0].value;
+        dataUnit = results.value.timeSeries[i].variable.variableName;
+        dateTime = results.value.timeSeries[i].values[0].value[0].dateTime
+        if ($( "#" + stationCode).text().indexOf(dataLoc) == -1) {
+            $( "#" + stationCode ).append("<h3>" + dataLoc + "</h3>"); 
+        }
+        $( "#" + stationCode ).append("<h5>" + dataPoint + " " + dataUnit + "</h5>");
+        $( "#" + stationCode ).append("<h5>" + " " + dateTime + "</h5>");
+    };
+});
+
+function buildFrame(results) {
+    var r=[];
+    for (i = 0; i < results.value.timeSeries.length; ++i) {
+        stationCode = results.value.timeSeries[i].sourceInfo.siteCode[0].value;
+        r.push(stationCode);
+    };
+    var u = r.filter( onlyUnique );
+    console.log(u);
+    for (i=0; i < u.length; ++i) {
+        var newdiv = $ ( "<div></div>" );
+        newdiv.attr( "id", u[i]);
+        newdiv.addClass( "panel" );
+        $( "#row1" ).append(newdiv);
+    };
+} 
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
